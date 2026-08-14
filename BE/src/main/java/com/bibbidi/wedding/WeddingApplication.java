1285c331bca806ed10eb6cca3ae1afc019775457
kdongsu5509 <kod66170@gmail.com>
2026-08-14 14:39:07 +0900

    fix : fix typo of backend group name
    change `bibbdi` to `bibbidi`. our origin team name is bibidi.

--- BE/src/main/java/com/bibbidi/wedding/WeddingApplication.java
+++ BE/src/main/java/com/bibbidi/wedding/WeddingApplication.java
@@ -1,4 +1,4 @@
-package com.bibbdi.wedding;
+package com.bibbidi.wedding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BE/src/test/java/com/bibbidi/wedding/WeddingApplicationTests.java
+++ BE/src/test/java/com/bibbidi/wedding/WeddingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bibbdi.wedding;
+package com.bibbidi.wedding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
